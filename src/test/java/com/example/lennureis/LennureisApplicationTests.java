dddbd497d38d7117603739626e729305eafbc7f8
Oskar Pärsim <oskarpar@ut.ee>
2025-03-05 16:17:17 +0200

    Initial commit

---
+++ src/test/java/com/example/lennureis/LennureisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lennureis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennureisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
